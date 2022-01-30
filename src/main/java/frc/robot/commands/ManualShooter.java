c8f98533f80fbc28fb9913705744c7438f4cb77d
lonaim <shakednaim2006@gmail.com>
2022-01-30 21:25:00 +0200

    chang the shooterSpeed to double

---
+++ src/main/java/frc/robot/commands/ManualShooter.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Shooter;
+
+public class ManualShooter extends CommandBase {
+  /** Creates a new ManualShooter. */
+  private Shooter shooter;
+  private double shooterSpeed;
+  
+  public ManualShooter(Shooter shooter, Double shooterSpeed) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.shooter = shooter;
+    this. shooterSpeed = shooterSpeed;
+    addRequirements(shooter);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    shooter.setSpeed(shooterSpeed);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Shooter extends SubsystemBase {
+  /** Creates a new Shooter. */
+
+  //create the shooter motor
+  private WPI_TalonFX m_shooter;
+
+  public Shooter() {
+      this.m_shooter = new WPI_TalonFX(Constants.ShooterConstants.ShooterPort);
+  }
+
+  public void setSpeed (double shooterSpeed){
+   //give m_shooter speed
+    m_shooter.set(shooterSpeed);
+  }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
