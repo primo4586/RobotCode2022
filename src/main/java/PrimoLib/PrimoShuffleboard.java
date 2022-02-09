7548b9c507e5acbd0d0bc35a794f59e11b271ff8
unknown <tamar4200@gmail.com>
2022-02-09 21:32:08 +0200

    changes primoshuffleboard not does not throw null pointer error

--- src/main/java/PrimoLib/PrimoShuffleboard.java
+++ src/main/java/PrimoLib/PrimoShuffleboard.java
@@ -1,15 +1,25 @@
 package PrimoLib;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 
 public class PrimoShuffleboard {
     
-    private static Map<String,PrimoTab> tabs;
+    private Map<String,PrimoTab> tabs;
+    private PrimoShuffleboard instance;
+    private PrimoShuffleboard(){
+        tabs = new HashMap<>();
+    }
 
+    public PrimoShuffleboard getInstance(){
+        if (instance == null)
+            instance = new PrimoShuffleboard();
+        return instance;
+    }
     // Adds or gets a new PrimoTab to avoid adding a tab that already exists and crashing the robot
-    public static PrimoTab getPrimoTab(String tabName) {
+    public PrimoTab getPrimoTab(String tabName) {
         if(tabs.containsKey(tabName))
             return tabs.get(tabName);
         
@@ -19,11 +29,9 @@ public class PrimoShuffleboard {
     }
 
     // Switches the dashboard to view a different tab, if it exists
-    public static void selectTab(String tabName) {
+    public void selectTab(String tabName) {
         if(tabs.containsKey(tabName))
             Shuffleboard.selectTab(tabName);
     }
-
-
 
 }
