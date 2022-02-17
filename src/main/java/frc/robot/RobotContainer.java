b2093933bb956cbea9d69a2aa8bec6237e45850f
Daniel <officialcodedevelop@gmail.com>
2022-02-17 23:49:27 +0200

    Added AlignByVision command, needs testing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,7 +16,6 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.commands.ClimbCommands.ManualMoveNextLevel;
 import frc.robot.commands.ClimbCommands.ManualRotateChain;
 import frc.robot.commands.DriverCommands.ArcadeDrive;
 import frc.robot.commands.IntakeCommands.ManualRoller;
---
+++ src/main/java/frc/robot/commands/DriverCommands/AlignByVision.java
@@ -0,0 +1,72 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.DriverCommands;
+
+import java.util.function.DoubleSupplier;
+
+import PrimoLib.PrimoShuffleboard;
+import PrimoLib.PrimoTab;
+import autonomous.PIDConfig;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Driver;
+
+public class AlignByVision extends CommandBase {
+
+  private Driver driver;
+  private PrimoTab tab;
+
+  private PIDConfig config;
+  private PIDController pidController;
+  private DoubleSupplier visionAngle;
+
+  private double angleError;
+  private double tolerance;
+  private double initalAngleOffset;
+  
+  /** Creates a new AlignByVision. */
+  public AlignByVision(Driver driver, DoubleSupplier visionAngle, double tolerance) {
+    addRequirements(driver);
+
+    this.angleError = visionAngle.getAsDouble();
+    this.visionAngle = visionAngle;
+    this.driver = driver;
+    this.pidController = config.getController();
+    this.tab = PrimoShuffleboard.getInstance().getPrimoTab("AlignByVision");
+    tab.getTab().add("PID", config);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    this.initalAngleOffset = driver.getYaw();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+    double output = pidController.calculate(driver.getYaw(), initalAngleOffset + visionAngle.getAsDouble());
+    this.angleError = initalAngleOffset + visionAngle.getAsDouble() - driver.getYaw();
+
+    tab.addEntry("PID Output").setNumber(output);
+    tab.addEntry("Angle Error").setNumber(angleError);
+    tab.addEntry("Limelight Angle").setNumber(visionAngle.getAsDouble());
+    driver.driveVelocity(-output, output);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    driver.driveVelocity(0, 0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return this.angleError <= tolerance;
+  }
+}
